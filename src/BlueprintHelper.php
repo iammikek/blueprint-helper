b68a168cdbfc37ff4ec44b81ca11703773414747
mike karthauser <mike@automica.io>
2025-02-09 17:15:23 +0000

    renames removes factory. moves methods from command into service class.

--- src/BlueprintHelper.php
+++ src/BlueprintHelper.php
@@ -2,4 +2,29 @@
 
 namespace Automica\BlueprintHelper;
 
-class BlueprintHelper {}
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+
+class BlueprintHelper extends Command {
+
+    public function command(string $string, array $params = []): void
+    {
+        $exitCode = $this->call($string, $params);
+
+        // Get the output of the command
+        $output = Artisan::output();
+
+        // Display the output to the console
+        $this->info($output);
+
+        // Optionally, handle the exit code
+        if ($exitCode !== 0) {
+            $this->error('The command did not execute successfully.');
+        }
+    }
+
+    public function reset(): void
+    {
+        copy('https://raw.githubusercontent.com/laravel/laravel/refs/heads/11.x/routes/web.php', base_path().'/routes/web.php');
+    }
+}
--- src/Commands/BlueprintRebuildCommand.php
+++ src/Commands/BlueprintRebuildCommand.php
@@ -4,10 +4,9 @@ declare(strict_types=1);
 
 namespace Automica\BlueprintHelper\Commands;
 
-use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Artisan;
+use Automica\BlueprintHelper\BlueprintHelper;
 
-class BlueprintRebuildCommand extends Command
+class BlueprintRebuildCommand extends BlueprintHelper
 {
     public $signature = 'blueprint:rebuild';
 
@@ -28,26 +27,5 @@ class BlueprintRebuildCommand extends Command
         $this->comment('All done');
 
         return self::SUCCESS;
-    }
-
-    private function command(string $string, array $params = []): void
-    {
-        $exitCode = $this->call($string, $params);
-
-        // Get the output of the command
-        $output = Artisan::output();
-
-        // Display the output to the console
-        $this->info($output);
-
-        // Optionally, handle the exit code
-        if ($exitCode !== 0) {
-            $this->error('The command did not execute successfully.');
-        }
-    }
-
-    private function reset(): void
-    {
-        copy('https://raw.githubusercontent.com/laravel/laravel/refs/heads/11.x/routes/web.php', base_path().'/routes/web.php');
     }
 }
