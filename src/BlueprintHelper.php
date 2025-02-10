069bfea0c44fa02b506907a6fbde1085e5637063
mike karthauser <mike@automica.io>
2025-02-10 18:46:47 +0000

    adds means to clear out migrations

--- src/BlueprintHelper.php
+++ src/BlueprintHelper.php
@@ -26,5 +26,13 @@ class BlueprintHelper extends Command
     public function reset(): void
     {
         copy('https://raw.githubusercontent.com/laravel/laravel/refs/heads/11.x/routes/web.php', base_path().'/routes/web.php');
+
+        // delete migrations
+        array_map('unlink', array_filter((array) glob(base_path()."/database/migrations/*")));
+
+        // copy across what we need
+        copy('https://raw.githubusercontent.com/laravel/laravel/refs/heads/11.x/database/migrations/0001_01_01_000000_create_users_table.php', base_path().'/database/migrations/0001_01_01_000000_create_users_table.php');
+        copy('https://raw.githubusercontent.com/laravel/laravel/refs/heads/11.x/database/migrations/0001_01_01_000001_create_cache_table.php', base_path().'/database/migrations/0001_01_01_000001_create_cache_table.php');
+        copy('https://raw.githubusercontent.com/laravel/laravel/refs/heads/11.x/database/migrations/0001_01_01_000002_create_jobs_table.php', base_path().'/database/migrations/0001_01_01_000002_create_jobs_table.php');
     }
 }
